036cb55622673c4ea66aae6e695562b6a9f361c3
lcskdc <lcskdc@gmail.com>
2014-06-11 00:44:28 -0300

    Add ação upload de imagem

--- WebContent/js/cadastro_produto.js
+++ WebContent/js/cadastro_produto.js
@@ -5,4 +5,11 @@ $(function()
 	$('#btnCadastrar').click(function() {
 		document.formCadastroProduto.submit();
 	});
+        
+        $('#txtPreco').mask('000.000.000.000.000,00', {reverse: true});
+        
+        $('#btn-add-image').click(function(){
+           $('#arquivo').click(); 
+        });
+        
 });
--- src/br/com/senac/mol/servlets/CadastroProdutoServlet.java
+++ src/br/com/senac/mol/servlets/CadastroProdutoServlet.java
@@ -15,72 +15,94 @@ import br.com.senac.mol.entidades.Produto;
 import br.com.senac.mol.entidades.Usuario;
 import br.com.senac.mol.models.MensagensSessao;
 import br.com.senac.mol.persistencia.ProdutoDAO;
+import java.io.File;
+import java.util.List;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 @WebServlet("/CadastroProdutoServlet")
-public class CadastroProdutoServlet extends HttpServlet
-{
-	private static final long serialVersionUID = 1L;
+public class CadastroProdutoServlet extends HttpServlet {
 
-	public CadastroProdutoServlet()
-	{
-		super();
-	}
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-	}
+    public CadastroProdutoServlet() {
+        super();
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		String descricao = request.getParameter("txtDescricao").trim();
-		String resenha = request.getParameter("txtResenha").trim();
-		String strPreco = request.getParameter("txtPreco").replace(".", "").replace(",", ".").trim();
-		BigDecimal preco = null;
-		Usuario usuario;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
 
-		HttpSession sessao = request.getSession();
-		MensagensSessao mensagens = new MensagensSessao();
-		sessao.setAttribute("mensagens", mensagens);
-		usuario = (Usuario) sessao.getAttribute("usuario");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		// verificando entradas do usuario:
-		String erro = "";
-		boolean precoOk = true;
-		try {
-			preco = BigDecimal.valueOf(Float.parseFloat(strPreco));
-		} catch (Exception ex) {
-			precoOk = false;
-		}
+//        if (ServletFileUpload.isMultipartContent(request)) {
+//            try {
+//                List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+//
+//                for (FileItem item : multiparts) {
+//                    if (!item.isFormField()) {
+//                        String name = new File(item.getName()).getName();
+//                        item.write(new File("../" + File.separator + name));
+//                    }
+//                }
+//
+//                //File uploaded successfully
+//                request.setAttribute("message", "File Uploaded Successfully");
+//            } catch (Exception ex) {
+//                request.setAttribute("message", "File Upload Failed due to " + ex);
+//            }
+//
+//        }
 
-		boolean ok = true;
-		if (descricao.length() < 4) { // descricao com no minimo 3 caracteres.
-			ok = false;
-			erro = "Descrição deve ter 3 ou mais caracteres";
-		} else if (!precoOk) {
-			ok = false;
-			erro = "Valor para o preço invalido";
-		}
+        String descricao = request.getParameter("txtDescricao").trim();
+        String resenha = request.getParameter("txtResenha").trim();
+        String strPreco = request.getParameter("txtPreco").replace(".", "").replace(",", ".").trim();
+        BigDecimal preco = null;
+        Usuario usuario;
 
-		if (!ok) {
-			mensagens.add("erro", erro);
-			mensagens.add("descricao", descricao);
-			mensagens.add("resenha", resenha);
-			mensagens.add("preco", strPreco);
-			response.sendRedirect(request.getContextPath() + "/cadastro_produto.jsp");
-			return;
-		}
+        HttpSession sessao = request.getSession();
+        MensagensSessao mensagens = new MensagensSessao();
+        sessao.setAttribute("mensagens", mensagens);
+        usuario = (Usuario) sessao.getAttribute("usuario");
 
-		Produto produto = new Produto();
-		produto.setDescricao(descricao);
-		produto.setResenha(resenha);
-		produto.setPreco(preco);
-		produto.setDataCadastro(new Date());
-		produto.setDataUltimaAtualizacao(new Date());
-		produto.setUsuario(usuario);
+        // verificando entradas do usuario:
+        String erro = "";
+        boolean precoOk = true;
+        try {
+            preco = BigDecimal.valueOf(Float.parseFloat(strPreco));
+        } catch (Exception ex) {
+            precoOk = false;
+        }
 
-		ProdutoDAO dao = new ProdutoDAO();
-		dao.insert(produto);
+        boolean ok = true;
+        if (descricao.length() < 4) { // descricao com no minimo 3 caracteres.
+            ok = false;
+            erro = "Descrição deve ter 3 ou mais caracteres";
+        } else if (!precoOk) {
+            ok = false;
+            erro = "Valor para o preço invalido";
+        }
 
-		response.sendRedirect(request.getContextPath() + "/index.jsp");
-	}
+        if (!ok) {
+            mensagens.add("erro", erro);
+            mensagens.add("descricao", descricao);
+            mensagens.add("resenha", resenha);
+            mensagens.add("preco", strPreco);
+            response.sendRedirect(request.getContextPath() + "/cadastro_produto.jsp");
+            return;
+        }
+
+        Produto produto = new Produto();
+        produto.setDescricao(descricao);
+        produto.setResenha(resenha);
+        produto.setPreco(preco);
+        produto.setDataCadastro(new Date());
+        produto.setDataUltimaAtualizacao(new Date());
+        produto.setUsuario(usuario);
+
+        ProdutoDAO dao = new ProdutoDAO();
+        dao.insert(produto);
+
+        response.sendRedirect(request.getContextPath() + "/index.jsp");
+    }
 }
